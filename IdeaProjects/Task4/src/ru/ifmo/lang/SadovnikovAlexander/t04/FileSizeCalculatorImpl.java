c853b4640fd1beafc97cc9b23ab0abf5ce926c6d
Alexander <Dongodfather@mail.ru>
2015-03-29 14:12:53 +0300

    Latest changes to Task 4

--- IdeaProjects/Task4/src/ru/ifmo/lang/SadovnikovAlexander/t04/FileSizeCalculatorImpl.java
+++ IdeaProjects/Task4/src/ru/ifmo/lang/SadovnikovAlexander/t04/FileSizeCalculatorImpl.java
@@ -7,6 +7,7 @@ import java.nio.file.attribute.BasicFileAttributes;
 public class FileSizeCalculatorImpl implements FileSizeCalculator {
     private static String mask = null;
     private int size;
+    int event = 0;
 
     public long getSize(String pathToDir, final String fileTemplate) throws IOException {
         mask = null;
@@ -14,8 +15,10 @@ public class FileSizeCalculatorImpl implements FileSizeCalculator {
         final Path input = Paths.get(pathToDir);
         if (fileTemplate.startsWith("*") && (!fileTemplate.endsWith("*"))) {
             mask = fileTemplate.substring(1, fileTemplate.length());
+            event = 1;
         } else if (fileTemplate.endsWith("*") && (!fileTemplate.startsWith("*"))) {
             mask = fileTemplate.substring(0, fileTemplate.lastIndexOf("*"));
+            event = 2;
         } else if (fileTemplate.startsWith("*") && fileTemplate.endsWith("*")) {
             mask = fileTemplate.substring(1, fileTemplate.lastIndexOf("*"));
         }
@@ -23,8 +26,19 @@ public class FileSizeCalculatorImpl implements FileSizeCalculator {
             public FileVisitResult visitFile(Path path, BasicFileAttributes attrs)
                     throws IOException {
                 if (mask != null) {
-                    if ((path.getFileName().toString()).contains(mask)) {
-                        size += Files.size(path);
+                    if (event == 1){
+                        if ((path.getFileName().toString()).endsWith(mask)) {
+                            size += Files.size(path);
+                        }
+                    }else if (event == 2) {
+                        if ((path.getFileName().toString()).startsWith(mask)) {
+                            size += Files.size(path);
+                        }
+                    }
+                    else{
+                        if ((path.getFileName().toString()).contains(mask)) {
+                            size += Files.size(path);
+                        }
                     }
                 } else {
                     if (fileTemplate.equals(path.getFileName().toString())) {
