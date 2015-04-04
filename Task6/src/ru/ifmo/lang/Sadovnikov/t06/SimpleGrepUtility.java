5919e325a03c485ddaddca91c17fb8ad8b2ea00e
Alexander <Dongodfather@mail.ru>
2015-04-04 17:21:56 +0300

    Changes to Task 6

--- Task6/src/ru/ifmo/lang/Sadovnikov/t06/SimpleGrepUtility.java
+++ Task6/src/ru/ifmo/lang/Sadovnikov/t06/SimpleGrepUtility.java
@@ -10,52 +10,56 @@ import java.util.regex.Pattern;
  * Created by alexkane on 3/21/15.
  */
 class SimpleGrepUtility implements Grep {
-    private List<String> result = new ArrayList<String>();
-    private StringBuilder theText;
+    private String[] lines;
 
     public SimpleGrepUtility(final InputStream sourceFileInputStream) throws IOException {
         BufferedInputStream fileReader = new BufferedInputStream(sourceFileInputStream);
-        StringBuilder builder = new StringBuilder();
         int character;
+        String theText = "";
         while ((character = fileReader.read()) != -1) {
-            builder.append((char) character);
+            theText += (char) character;
         }
+        this.lines = theText.split("\\n");
         fileReader.close();
-        this.theText = builder;
     }
 
     public List<String> findLines(String regex) throws IOException {
+        List<String> result = new ArrayList<String>();
         result.clear();
-        Pattern finalRegex = Pattern.compile(".*" + regex + ".*");
-        Matcher theMatcher = finalRegex.matcher(theText);
-        while (theMatcher.find()) {
-            result.add(theMatcher.group());
+        Pattern finalRegex = Pattern.compile(regex);
+        for (int i = 0; i < lines.length; i++) {
+            Matcher theMatcher = finalRegex.matcher(lines[i]);
+            if (theMatcher.find()) {
+                result.add(lines[i]);
+            }
         }
         return result;
     }
 
     @Override
     public List<String> findParts(String regex) throws IOException {
+        List<String> result = new ArrayList<String>();
         result.clear();
         Pattern finalRegex = Pattern.compile(regex);
-        Matcher theMatcher = finalRegex.matcher(theText);
-        while (theMatcher.find()) {
-            result.add(theMatcher.group());
+        for (int i = 0; i < lines.length; i++) {
+            Matcher theMatcher = finalRegex.matcher(lines[i]);
+            if (theMatcher.find()) {
+                result.add(theMatcher.group());
+            }
         }
         return result;
     }
 
     @Override
     public List<String> findInvertMatch(String regex) throws IOException {
+        List<String> result = new ArrayList<String>();
         result.clear();
         Pattern finalRegex = Pattern.compile(".*" + regex + ".*");
-        String[] lines = theText.toString().split("\\n");
-        for(int i = 0; i < lines.length; i++){
-            result.add(lines[i]);
-        }
-        Matcher theMatcher = finalRegex.matcher(theText);
-        while (theMatcher.find()) {
-            result.remove(theMatcher.group());
+        for (int i = 0; i < lines.length; i++) {
+            Matcher theMatcher = finalRegex.matcher(lines[i]);
+            if (!theMatcher.find()) {
+                result.add(lines[i]);
+            }
         }
         return result;
     }
@@ -64,7 +68,7 @@ class SimpleGrepUtility implements Grep {
         if (args.length == 3){
             String regex = args[1];
             InputStream sourceFile = new FileInputStream(args[2]);
-            if (args[0].equals("[-o]")){
+            if (args[0].equals("-o")){
                 System.out.println(new SimpleGrepUtility(sourceFile).findParts(regex));
             }else {
                 System.out.println(new SimpleGrepUtility(sourceFile).findInvertMatch(regex));
